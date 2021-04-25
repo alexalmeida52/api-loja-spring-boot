1c973cfb5c956dcb6273b4ca95afbed782cc28fe
Alex Alves <jhosealex@gmail.com>
2021-04-25 17:21:58 -0300

    Ajustes no endpoint /categoria/{id}

--- src/main/java/com/alexalves/cursomc/services/CategoriaService.java
+++ src/main/java/com/alexalves/cursomc/services/CategoriaService.java
@@ -7,15 +7,18 @@ import org.springframework.stereotype.Service;
 
 import com.alexalves.cursomc.domain.Categoria;
 import com.alexalves.cursomc.repositories.CategoriaRepository;
+import com.alexalves.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
-	
+
 	@Autowired
 	private CategoriaRepository repo;
-	
+
 	public Categoria find(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
+
 }
