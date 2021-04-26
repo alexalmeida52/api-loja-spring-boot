85bb82579ed0ed7fc9ff8fec9386140ae94e4666
Alex Alves <jhosealex@gmail.com>
2021-04-25 22:13:14 -0300

    Estado e cidade. Obs.: Não criei uma lista de cidades no dominio do Estado

---
+++ src/main/java/com/alexalves/cursomc/repositories/CidadeRepository.java
@@ -0,0 +1,11 @@
+package com.alexalves.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.alexalves.cursomc.domain.Cidade;
+
+@Repository
+public interface CidadeRepository extends JpaRepository<Cidade, Integer> {
+	
+}
