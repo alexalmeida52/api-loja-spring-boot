3fd6705bc4504e6d084928b92d5f1cd7f2a956a3
Alex Alves <jhosealex@gmail.com>
2021-04-27 22:47:14 -0300

    Endpoint /clientes/{id} disponivel

---
+++ src/main/java/com/alexalves/cursomc/services/ClienteService.java
@@ -0,0 +1,23 @@
+package com.alexalves.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.alexalves.cursomc.domain.Cliente;
+import com.alexalves.cursomc.repositories.ClienteRepository;
+import com.alexalves.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+    @Autowired
+    private ClienteRepository repo;
+
+    public Cliente find(Integer id) {
+        Optional<Cliente> obj = repo.findById(id);
+
+        return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+    }
+}
