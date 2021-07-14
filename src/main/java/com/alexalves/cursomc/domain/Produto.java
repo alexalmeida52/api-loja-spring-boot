932be12fe927d3fa095b1942218db36ad38f340a
Alex Alves <jhosealex@gmail.com>
2021-07-14 16:57:23 -0300

    Endpoint /pedidos/{id} disponibilizado

--- src/main/java/com/alexalves/cursomc/domain/Produto.java
+++ src/main/java/com/alexalves/cursomc/domain/Produto.java
@@ -16,6 +16,7 @@ import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 public class Produto implements Serializable {
@@ -36,6 +37,7 @@ public class Produto implements Serializable {
 	)
 	private List<Categoria> categorias  = new ArrayList<>();
 	
+	@JsonIgnore
 	@OneToMany(mappedBy="id.produto")
 	private Set<ItemPedido> itens = new HashSet<>();
 	
@@ -87,6 +89,16 @@ public class Produto implements Serializable {
 	public void setItens(Set<ItemPedido> itens) {
 		this.itens = itens;
 	}
+
+	@JsonIgnore
+	public List<Pedido> getPedidos() {
+		List<Pedido> lista = new ArrayList<>();
+		for (ItemPedido x : itens) {
+			lista.add(x.getPedido());
+		}
+		return lista;
+	}
+
 	
 	@Override
 	public int hashCode() {
